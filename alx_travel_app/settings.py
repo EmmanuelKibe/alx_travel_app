2e4dfd1fb38b58332859642978267273bc5c942f
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-16 22:33:03 +0300

    Replaced the secret key with the variable in the .env file so that django reads it safely

--- alx_travel_app/settings.py
+++ alx_travel_app/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('django-insecure-_m=h)tcrk_4w)$+xi%x^6iss*6t2$%-^nfg-9wx8%xy&8w=0+c')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
