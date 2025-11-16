2d232738c43e3278a9b317e5f1a1b55046a5d369
Emmanuel <emmanuelkibe148@gmail.com>
2025-11-16 23:31:32 +0300

    views

--- listings/views.py
+++ listings/views.py
@@ -4,9 +4,6 @@ from .models import Listing
 from .serializers import ListingSerializer
 
 class ListingViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint for listing all travel listings.
-    """
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
     permission_classes = [permissions.AllowAny]  # For testing, change to IsAuthenticated later
